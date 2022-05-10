bbfc6955bbcbd978bad0678b04591a2a11ace963
maxj723 <46984489+maxj723@users.noreply.github.com>
2022-05-10 12:44:06 -0500

    Delete Advent of Code/dive directory

--- Advent of Code/dive/Main.java
+++
@@ -1,8 +0,0 @@
-class Main {
-  public static void main(String[] args) {
-    Moves moves = new Moves("dive.txt");
-    moves.getNewDepth();
-    moves.getNewHorz();
-    System.out.println(moves.getProduct());
-  }
-}
